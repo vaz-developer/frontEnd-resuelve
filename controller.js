7575fd21f898fa1ce321b4cfae7a4ad51122083f
vaz-developer <vaz-developer@gmail.com>
2018-01-25 18:35:32 -0600

    change braces for ng-bind

--- controller.js
+++ controller.js
@@ -1,35 +1,32 @@
-angular.module("MyFirtsApp",[]) 
-	.controller("FirtsController",["$scope",function ($scope){
+angular.module("tableApp",[]) 
+	.controller("tableController",["$scope",function ($scope){
 
 		$scope.items =  {
-			conceptos: [{
-				cantidad: 0,
-				unidades: 0,
-				precio: 0,
+			concepts: [{
+				amount: 0,
+				units: 0,
+				price: 0,
 				total: 0
 			}]
 		};
-
-
-		//Agregamos metodos para nuestro controlador
-		$scope.agregarConcepto = function(){
-			//push agrega un nuevo elemento a un arreglo
-			$scope.items.conceptos.push({
-				cantidad: 0,
-				unidades: 0,
-				precio: 0,
+		//Push new concept
+		$scope.newConcept = function(){
+			$scope.items.concepts.push({
+				amount: 0,
+				units: 0,
+				price: 0,
 				total: 0
 			});
 		}; 
-
-		$scope.erase = function(borrarConcepto){
-			 $scope.items.conceptos.splice(borrarConcepto, 1);
+		//Delete concept
+		$scope.erase = function(eraseConcept){
+			 $scope.items.concepts.splice(eraseConcept, 1);
 		}
 
 		$scope.subtotal = function() {
         	var subtotal = 0;
-        	angular.forEach(this.items.conceptos, function(typeConcepto) {
-            	subtotal += typeConcepto.cantidad * typeConcepto.precio;
+        	angular.forEach(this.items.concepts, function(typeConcept) {
+            	subtotal += typeConcept.amount * typeConcept.price;
         	})
 
         return subtotal;
